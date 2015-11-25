3f59e79169e39859f00c6e51f505cae8ab6e7e15
andrei <andreic.nica@gmail.com>
2015-11-25 17:30:58 +0200

    added pd settings

--- src/ioData.py
+++ src/ioData.py
@@ -3,6 +3,10 @@ __author__ = 'andrei'
 import numpy as np
 import pandas as pd
 
+pd.set_option('display.width', 5000)
+pd.set_option('display.max_columns', 1000)
+pd.set_option('display.max_colwidth', 400)
+
 def readData(filename):
     df = pd.read_json(filename)
     assert isinstance(df, pd.DataFrame)
