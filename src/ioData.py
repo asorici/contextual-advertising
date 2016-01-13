557dcc891a40fcec4754c7e239a18a8848c8b346
Alex Sorici <alex.sorici@gmail.com>
2016-01-13 17:32:35 +0200

    Added functions for saving raw dataset in lower case format

--- src/ioData.py
+++ src/ioData.py
@@ -17,4 +17,29 @@ def readData(filename):
 def writeData(df, filename):
     assert isinstance(df, pd.DataFrame)
     df.to_json(filename)
-    return
+
+def apply_lower(field):
+    if isinstance(field, basestring):
+        return field.lower()
+    elif isinstance(field, (list, np.ndarray)):
+        return map(apply_lower, field)
+    else:
+        return field
+
+def to_lower(row, columns):
+    # for col in columns:
+    #     field = row[col]
+    #     print field
+    #
+    #     apply_lower(field)
+
+    return row.apply(apply_lower)
+
+def lowerCaseDataset(df, filename):
+    columns = df.columns
+
+    def wrapper_func(row):
+        return to_lower(row, columns)
+
+    new_df = df.apply(wrapper_func, axis=1)
+    writeData(new_df, filename)
